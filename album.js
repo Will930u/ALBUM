0641289309d031af8828a209285f76bc76e50e8e
Imperial <150773885+Will930u@users.noreply.github.com>
2026-09-13 19:52:05 -0400

    Update Supabase connection configuration

--- album.js
+++ album.js
@@ -1,6 +1,6 @@
-﻿// CONFIGURACIÓN DE CONEXIÓN CON TU SERVIDOR DE SUPABASE
-const SUPABASE_URL = "https://ddbdemxrntjqncetyrnr.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkYmRlbXhybnRqcW5jZXR5cm5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MDYyNzQsImV4cCI6MjEwNDE4MjI3NH0.caXUy6CeiEMIcS4cQoRjZ0QEOaq7-EuIOP9UepXHALs";
+// CONFIGURACIÓN DE CONEXIÓN CON TU SERVIDOR DE SUPABASE
+const SUPABASE_URL = "https://zrxmjpgnwqxyzdjnnwae.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyeG1qcGdud3F4eXpkam5ud2FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MTk4MzIsImV4cCI6MjEwNDE5NTgzMn0.5ZLVDAUHXpITQs2GpDhtGAXTphZUZ7gaE4ElIHPsaAo";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // ID del jugador de pruebas (En la Fase 4 lo tomaremos automáticamente desde el Telegram ID)
